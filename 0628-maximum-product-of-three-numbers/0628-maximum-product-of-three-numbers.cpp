7926ab4578c2eafc12abb14711a15d03718507e4
vishalsukhwall <vishalsukhwal33@gmail.com>
2026-07-28 18:23:16 +0530

    Time: 0 ms (100%), Space: 31.7 MB (13.31%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -2,18 +2,41 @@ class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
         int n = nums.size();
-        int arr1 = 1;
-        int arr2 = 1;
-        int ans = 1;
-        
-        sort(nums.begin() , nums.end());
 
-        arr1 *= nums[n-1] * nums[n-2] * nums[n-3];
+        int max1 = INT_MIN;
+        int max2 = INT_MIN;
+        int max3 = INT_MIN;
 
-        arr2 *= nums[0] * nums[1] * nums[n-1];
+        int min1 = INT_MAX;
+        int min2 = INT_MAX;
 
-        ans = max(arr1 , arr2);
+        for(int i=0; i<n; i++){
+            if(nums[i] > max1){
+                max3 = max2;
+                max2 = max1;
+                max1 = nums[i];
+            }
+            else if(nums[i] > max2){
+                max3 = max2;
+                max2 = nums[i];
+            }
+            else if(nums[i] > max3){
+                max3 = nums[i];
+            }
+            
+            if(nums[i] < min1){
+                min2 = min1;
+                min1 = nums[i];
+            }
+            else if(nums[i] < min2){
+                min2 = nums[i];
+            }
+        }
 
-        return ans;   
+        int arr1 = min1 * min2 * max1;
+        int arr2 = max1 * max2 * max3;
+
+        int ans = max(arr1 , arr2);
+        return ans;
     }
 };
