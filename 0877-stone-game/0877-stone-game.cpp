3160a950d6b86f6273d6204f4bbaddac747715d0
vishalsukhwall <vishalsukhwal33@gmail.com>
2026-08-03 00:14:16 +0530

    Time: 8 ms (31.43%), Space: 11.5 MB (36.14%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
+    int t[501][501];
+    int solve(int i , int j , vector<int>& nums){
+        int n = nums.size();
+
+        if(i > j){
+            return 0;
+        }
+
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+
+        int take_1 = nums[i] + min(solve(i+2 , j , nums) , solve(i+1 , j-1 , nums));
+        int take_2 = nums[j] + min(solve(i+1 , j-1 , nums) , solve(i , j-2 , nums));
+
+        return t[i][j] = max(take_1 , take_2);
+    }
+
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+
+        memset(t , -1 , sizeof(t));
+
+        int sum = accumulate(begin(piles) , end(piles) , 0);
+
+        int alex = solve(0 , n-1 , piles);
+        int bob = sum - alex;
+
+        return alex > bob;
     }
 };
