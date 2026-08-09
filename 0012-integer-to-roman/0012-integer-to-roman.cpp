ccaea11c7d7cd0819cceaaf9725e69518df3f7f0
Vishal Sukhwal <vishalsukhwal33@gmail.com>
2026-08-09 23:53:13 +0530

    Time: 7 ms (28.48%), Space: 12.2 MB (50.25%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<pair<int, string>> storeIntRoman = {
-            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
-        };
-        
-        string result = "";
-        
-        for (const auto& it : storeIntRoman) {
-            while (num >= it.first) {
-                result += it.second;
-                num -= it.first;
+        vector<int> val{1000 , 900 , 500 , 400 , 100 , 90 , 50 , 40 , 10 , 9 , 5 , 4 , 1};
+        vector<string> sym{"M" , "CM" , "D" , "CD" , "C" , "XC" , "L" , "XL" , "X" , "IX" , "V" , "IV" , "I" };
+
+        string ans = "";
+        for(int i=0; i<13; i++){
+            if(num == 0){
+                break;
             }
+            
+            int time = num / val[i];
+            while(time--){
+                ans += sym[i];
+            }
+
+            num = num % val[i];
         }
-        
-        return result;
+        return ans;
     }
 };
