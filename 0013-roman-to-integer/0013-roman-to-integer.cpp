db2f7939a4ee969c95a5298e6699378e1e78689c
Vishal Sukhwal <vishalsukhwal33@gmail.com>
2026-08-10 01:15:22 +0530

    Time: 8 ms (42.06%), Space: 12.2 MB (68.08%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     int romanToInt(string s) {
-       unordered_map<char, int> romanMap = {
-            {'I', 1},
-            {'V', 5},
-            {'X', 10},
-            {'L', 50},
-            {'C', 100},
-            {'D', 500},
-            {'M', 1000}
-        };
-        
-        int total = 0;
-        int n = s.length();
-        
-        for (int i = 0; i < n; i++) {
-            if (i < n - 1 && romanMap[s[i]] < romanMap[s[i + 1]]) {
-                total -= romanMap[s[i]];
-            } 
+       unordered_map<char , int> m;
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
 
-            else {
-                total += romanMap[s[i]];
+        int ans = 0;
+        int n = s.length();
+
+        for(int i=0; i<s.length(); i++){
+            if(i < n-1 && m[s[i]] < m[s[i+1]]){
+                ans -= m[s[i]];
+            }
+            else{
+                ans += m[s[i]];
             }
         }
-        
-        return total; 
+        return ans;
     }
 };
