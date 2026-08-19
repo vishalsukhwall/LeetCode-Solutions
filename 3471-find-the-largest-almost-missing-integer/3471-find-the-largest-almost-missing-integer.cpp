201fa45e05dce95434ce0952bd0f1c49fe2d17ba
Vishal Sukhwal <vishalsukhwal33@gmail.com>
2026-08-19 17:19:53 +0530

    Time: 0 ms (100%), Space: 29.2 MB (74.91%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
-
-        int n = nums.size();
-        if(n == k){
+       int n = nums.size();
+       if(k == n){
             return *max_element(nums.begin() , nums.end());
-        }
+       }
 
-        unordered_map<int , int> count;
-        for(int num : nums){
+       unordered_map<int , int> count;
+       for(int num : nums){
             count[num]++;
-        }
-
-        if(k == 1){
-            int max_val = -1;
-            for(auto& pair : count){
+       }
+       int max_val = -1;
+       
+       if(k == 1){
+            for(auto pair : count){
                 if(pair.second == 1){
                     max_val = max(max_val , pair.first);
                 }
             }
-            return max_val;
-        }
-
-        int max_val = -1;
-        if(count[nums[0]] == 1){
-             max_val = max(max_val , nums[0]);
-        }
-
-        if(count[nums.back()] == 1){
-            max_val = max(max_val , nums.back());
-        }
-
         return max_val;
+       }
+
+       if(count[nums[0]] == 1){
+        max_val = max(nums[0] , max_val);
+       }
+
+       if(count[nums.back()] == 1){
+        max_val = max(nums.back() , max_val);
+       }
+       return max_val;
     }
 };
