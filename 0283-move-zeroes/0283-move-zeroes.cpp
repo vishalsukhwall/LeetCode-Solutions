0e6ba155750d701ea7e65444ae5952a5357aa331
Vishal Sukhwal <vishalsukhwal33@gmail.com>
2026-08-23 23:45:55 +0530

    Time: 0 ms (100%), Space: 24.6 MB (5.48%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,12 +3,27 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
 
-        int j = 0;
+        // int j = 0;
+        // for(int i=0; i<n; i++){
+        //     if(nums[i] != 0){
+        //         swap(nums[i] , nums[j]);
+        //         j++;
+        //     }
+        // }
+
+        vector<int> temp;
         for(int i=0; i<n; i++){
             if(nums[i] != 0){
-                swap(nums[i] , nums[j]);
-                j++;
+                temp.push_back(nums[i]);
             }
+        }
+
+        for(int i=0; i<temp.size(); i++){
+            nums[i] = temp[i];
+        }
+
+        for(int i=temp.size(); i<n; i++){
+            nums[i] = 0;
         }
     }
 };
