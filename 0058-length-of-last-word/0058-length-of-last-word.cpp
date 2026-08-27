c84f550d0f0e9790efa2a3b6f655cc6626f74c0e
Vishal Sukhwal <vishalsukhwal33@gmail.com>
2026-08-28 00:01:49 +0530

    Time: 2 ms (3.45%), Space: 8.8 MB (92.63%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -2,16 +2,32 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int n = s.size();
+        int i = n-1;
+
         int count = 0;
 
-        for(int i=n-1; i>=0; i--){
-            if(s[i] != ' '){
-                count++;
-            }
-            else if(count > 0){
-                break;
-            }
+        while(i >= 0 && s[i] == ' '){
+            i--;
         }
+
+        while(i >=0 && s[i] != ' '){
+            count++;
+            i--;
+        }
+
         return count;
+
+        // int n = s.size();
+        // int count = 0;
+
+        // for(int i=n-1; i>=0; i--){
+        //     if(s[i] != ' '){
+        //         count++;
+        //     }
+        //     else if(count > 0){
+        //         break;
+        //     }
+        // }
+        // return count;
     }
 };
