3878c0615e8b85635cfaed77753cb19ab9ee92b0
Vishal Sukhwal <vishalsukhwal33@gmail.com>
2026-08-30 23:38:13 +0530

    Time: 0 ms (100%), Space: 92.1 MB (58.82%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
         int n = nums.size();
-        int minIdx = 0;
-        int maxIdx = 0;
+        int minidx = 0;
+        int maxidx = 0;
 
         for(int i=0; i<n; i++){
-            if(nums[minIdx] > nums[i]) minIdx = i;
-            if(nums[maxIdx] < nums[i]) maxIdx = i;
+            if(nums[i] > nums[maxidx]) maxidx = i;
+            if(nums[i] < nums[minidx]) minidx = i;
+        } 
+
+        if(minidx > maxidx){
+            swap(minidx , maxidx);
         }
 
-        if(minIdx > maxIdx) {
-            swap(minIdx , maxIdx);
-        }
+        int opt1 = maxidx + 1;
+        int opt2 = n - minidx;
+        int opt3 = (minidx + 1) + (n - maxidx);
 
-        int opt_1 = maxIdx + 1;
-        int opt_2 = n - minIdx;
-        int opt_3 = (minIdx + 1) + (n - maxIdx);
-
-        return min({opt_1 , opt_2 , opt_3}); 
+        return min({opt1 , opt2 , opt3});
     }
 };
