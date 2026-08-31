8568697acfe01108adeb3fbbc9c5589d1d7d2f49
Vishal Sukhwal <vishalsukhwal33@gmail.com>
2026-08-31 19:09:15 +0530

    Time: 7 ms (18.24%), Space: 9.6 MB (92.36%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin() , s.end());
+        sort(t.begin() , t.end());
+
+        if(s.size() == t.size()){
+            for(int i=0; i<s.size(); i++){
+                if(s[i] != t[i]){
+                    return false;
+                }
+            }
+        }
+        else{
+            return false;
+        }
+    
+    return true;
+    }
+};
