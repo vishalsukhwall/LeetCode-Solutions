5714c6d966a330d333d647876cb1732b29e01dfa
Vishal Sukhwal <vishalsukhwal33@gmail.com>
2026-09-06 00:13:20 +0530

    Time: 0 ms (100%), Space: 15 MB (26.06%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,32 +11,57 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int length = 0;
         ListNode* temp = head;
 
-        while(temp != NULL){
-            length += 1;
+        for(int i=1; i<=n; i++){
             temp = temp->next;
         }
 
-        if(length == n){
+        if(temp == NULL){
             ListNode* temp = head->next;
             delete head;
             return temp;
         }
 
-        int travel = length - n;
-        ListNode* prev = NULL;
-        ListNode* temp1 = head;
-
-        while(travel--){
-            prev = temp1;
-            temp1 = temp1->next;
+        ListNode* prev = head;
+        while(temp != NULL && temp->next != NULL){
+            prev = prev->next;
+            temp = temp->next;
         }
 
-        prev->next = temp1->next;
-        delete temp1;
+        ListNode* delNode = prev->next;
+        prev->next = prev->next->next;
 
+        delete delNode;
+    
         return head;
+
+        // int length = 0;
+        // ListNode* temp = head;
+
+        // while(temp != NULL){
+        //     length += 1;
+        //     temp = temp->next;
+        // }
+
+        // if(length == n){
+        //     ListNode* temp = head->next;
+        //     delete head;
+        //     return temp;c
+        // }
+
+        // int travel = length - n;
+        // ListNode* prev = NULL;
+        // ListNode* temp1 = head;
+
+        // while(travel--){
+        //     prev = temp1;
+        //     temp1 = temp1->next;
+        // }
+
+        // prev->next = temp1->next;
+        // delete temp1;
+
+        // return head;
     }
 };
