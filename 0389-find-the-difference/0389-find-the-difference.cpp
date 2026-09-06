d323f010657100dc4b1e6ee88871a14ffd8e0ca8
Vishal Sukhwal <vishalsukhwal33@gmail.com>
2026-09-06 23:01:23 +0530

    Time: 0 ms (100%), Space: 9.2 MB (78.09%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int m = s.size();
-        int n = t.size();
+        // unordered_map<char , int>mp;
 
-        unordered_map<char , int>mp;
+        // for(char ch : s) mp[ch]++;
+        // for(char ch : t) mp[ch]--;
 
-        for(char ch : s) mp[ch]++;
-        for(char ch : t) mp[ch]--;
+        // for(auto pair : mp){
+        //     if(pair.second < 0){
+        //         return pair.first;
+        //     }
+        // }
 
-        for(auto pair : mp){
-            if(pair.second < 0){
-                return pair.first;
-            }
-        }
+        // return ' ';
+        int sum_t = 0;
+        int sum_s = 0; 
 
-        return ' ';
+        for(char c : s) sum_s += c;
+        for(char c : t) sum_t += c;
+
+        return char(sum_t - sum_s);
     }
 };
