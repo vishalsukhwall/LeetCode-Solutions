c78e7da32ac4c8afe19e4d2718e0b1f7a113beec
Vishal Sukhwal <vishalsukhwal33@gmail.com>
2026-09-07 23:53:27 +0530

    Time: 1 ms (65.57%), Space: 165.7 MB (83.41%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int min_odd = INT_MAX;
-        int min_even = INT_MAX;
-        
-        for (int x : nums1) {
-            if (x % 2 != 0) {
-                min_odd = min(min_odd, x);
-            } else {
-                min_even = min(min_even, x);
-            }
-        }
-        
-        // If all elements are already the same parity (all odd or all even)
-        if (min_odd == INT_MAX || min_even == INT_MAX) {
+        int minEl = *min_element(begin(nums1) , end(nums1));
+
+        if(minEl % 2 == 1){
             return true;
         }
-        
-        for (int x : nums1) {
-            if (x % 2 == 0) {
-                // An even number must be greater than the minimum odd number
-                // so we can subtract the min_odd to make it odd.
-                if (x < min_odd) {
-                    return false;
-                }
+
+        for(int &num : nums1){
+            if(num % 2 == 1){
+                return false;
             }
         }
-        
         return true;
     }
 };
