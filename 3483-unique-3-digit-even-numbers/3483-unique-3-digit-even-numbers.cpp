7b7a05ed680b9be1cb156e08e9beb458d5f25eb8
Vishal Sukhwal <vishalsukhwal33@gmail.com>
2026-09-12 11:18:11 +0530

    Time: 7 ms (65.65%), Space: 31.1 MB (33.46%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -2,25 +2,34 @@ class Solution {
 public:
     int totalNumbers(vector<int>& nums) {
         int n = nums.size();
-        unordered_set<int> uniqueNumbers;
+        unordered_set<int> s;
         int count = 0;
 
         for(int i=0; i<n; i++){
-            if(nums[i] == 0) continue;
-
+            if(nums[i] == 0){
+                continue;
+            }
             for(int j=0; j<n; j++){
-                if(j == i) continue;
-
-                for(int k=0; k<n; k++){
-
-                    if(k == i || k == j) continue;
-                    if(nums[k] % 2 == 0){
-                       int num = nums[i] * 100 + nums[j] * 10 + nums[k];
-                        uniqueNumbers.insert(num);
+                
+            //    if(i == j ){
+            //     continue;
+            //    }
+               for(int k=0; k<n; k++){
+                    if(i == j || j == k || i == k){
+                        continue;
                     }
-                }
+
+                    int num = (nums[i])*100 + (nums[j])*10 + (nums[k])*1;
+                    
+                   if(num % 2 == 0){
+                    if(s.find(num) == s.end()){
+                        count++;
+                    }
+                    s.insert(num);
+                   }
+               }
             }
         }
-        return uniqueNumbers.size();
+        return count;
     }
 };
