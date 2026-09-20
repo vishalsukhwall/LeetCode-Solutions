fb04d7aa2fceaf2a5ae5f5b8dcffd165aecf5a76
Vishal Sukhwal <vishalsukhwal33@gmail.com>
2026-09-20 23:01:58 +0530

    Time: 0 ms (100%), Space: 9.5 MB (95.77%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -4,19 +4,12 @@ public:
         int n = s.size();
 
         int sum = 0;
-        vector<int> deg(26 , 0);
-
-        int idx = 26;
-        for(int i=0; i<26; i++){
-            deg[i] = idx;
-            idx--;
-        }
 
         int a;
         for(int i=0; i<n; i++){
 
-            a = s[i] - 'a';
-            sum += deg[a] * (i+1);
+            a = 26 - (s[i] - 'a');
+            sum += a * (i+1);
 
         }
 
