e84331c71a65a496579ed7ff67a99042901cc514
Vishal Sukhwal <vishalsukhwal33@gmail.com>
2026-09-22 14:04:14 +0530

    Time: 8 ms (18.14%), Space: 71.4 MB (20.03%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,32 +3,28 @@ public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
 
-        int candy = 0;
-        int minrat = INT_MAX;
-
         vector<int> bag(n , 0);
-
-        for(int i=0; i<n; i++){
+        for(int i = 0; i < n; i++){
             bag[i] = 1;
         }
 
-        for(int i=1; i<n; i++){
+        for(int i = 1; i < n; i++){
             if(ratings[i] > ratings[i-1]){
                 bag[i] = bag[i-1] + 1;
             }
         }
 
-        for(int i=n-2; i>=0; i--){
+        for(int i = n-2; i >= 0; i--){
             if(ratings[i] > ratings[i+1]){
                 bag[i] = max(bag[i] , bag[i+1] + 1);
             }
         }
 
-        for(int i=0; i<bag.size(); i++){
-            candy += bag[i];
+        int total = 0;
+        for(int val : bag){
+            total += val;
         }
 
-        return candy;
-
+        return total;
     }
 };
