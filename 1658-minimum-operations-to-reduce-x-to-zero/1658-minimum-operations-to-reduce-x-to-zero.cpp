6c4e49f8207584d5d5d9064088a0d60dd8e09318
Vishal Sukhwal <vishalsukhwal33@gmail.com>
2026-09-23 12:55:37 +0530

    Time: 139 ms (21.1%), Space: 148.5 MB (19.86%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,63 +1,63 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
+        // int n = nums.size();
+        // int totalsum = accumulate(nums.begin() , nums.end() , 0);
+        // int target = totalsum - x;
+
+        // int left = 0 , longest = -1;
+        // int sum = 0;
+
+        // for(int right = 0; right < n; right++){
+        //     sum += nums[right];
+
+        //     while(left <= right && sum > target){
+        //         sum -= nums[left++];
+        //     }
+
+        //     if(sum == target){
+        //         longest = max(longest , right - left +1);
+        //     }
+        // }
+
+        // return longest == -1 ? -1 : n - longest;
+
         int n = nums.size();
-        int totalsum = accumulate(nums.begin() , nums.end() , 0);
-        int target = totalsum - x;
+        unordered_map<int , int> mp;
+        mp[0] = 0;
 
-        int left = 0 , longest = -1;
         int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += nums[i];
 
-        for(int right = 0; right < n; right++){
-            sum += nums[right];
+            if(sum > x) break;
+            mp[sum] = i+1;
+        }
 
-            while(left <= right && sum > target){
-                sum -= nums[left++];
-            }
+        int ans = INT_MAX;
+        
+        if(mp.count(x)){
+            ans = mp[x];
+        }
 
-            if(sum == target){
-                longest = max(longest , right - left +1);
+        int rightsum = 0;
+        for(int i=n-1; i>=0; i--){
+            rightsum += nums[i];
+
+            if(rightsum > x) break;
+
+            int rightidx = n - i;
+            int leftsum = x - rightsum;
+
+            if(mp.count(leftsum)){
+                int leftidx = mp[leftsum];
+
+                if(leftidx + rightidx <= n){
+                    ans = min(ans , rightidx +  leftidx);
+                }
             }
         }
 
-        return longest == -1 ? -1 : n - longest;
-
-        // int n = nums.size();
-
-        // unordered_map<int , int> mp;
-        // mp[0] = 0;
-
-        // int sum = 0;
-        // for(int i=0; i<n; i++){
-        //     sum += nums[i];
-
-        //     if(sum > x){
-        //         break;
-        //     }
-        //     mp[sum] = i+1;
-        // }
-
-        // int minnum = INT_MAX;
-
-        // if(mp.count(x)) minnum = mp[x];
-        // int rightsum = 0;
-
-        // for(int i = n-1; i >= 0; i--){
-        //     rightsum += nums[i];
-
-        //     if(rightsum > x) break;
-
-        //     int target = x - rightsum;
-
-        //     int right = n - 1;
-            
-        //     if(mp.count(target)){
-
-        //     int left = mp[target];
-        //         minnum = min(minnum , left + right);
-        //     }
-        // }
-
-        // return minnum;
+        return ans == INT_MAX ? -1 : ans;
     }
 };
