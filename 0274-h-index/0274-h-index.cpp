3f19553cb6dcbf69c4d42c7fa0178db6f4ec2980
Vishal Sukhwal <vishalsukhwal33@gmail.com>
2026-09-23 20:28:01 +0530

    Time: 0 ms (100%), Space: 12.3 MB (35.37%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations.rbegin() , citations.rend());
+        int n = citations.size();
+
+        sort(citations.begin() , citations.end());
 
         int h = 0;
-        while(h < citations.size() && citations[h] > h){
-            h++;
+        for(int i=0; i<n; i++){
+            int paper = n - i;
+
+            if(citations[i] >= paper){
+                h = paper;
+                break;
+            }
         }
 
         return h;
